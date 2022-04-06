75a6d0d12d18e5ed729a0bb18719c8a55ad483c2
auz <lankyauz@gmail.com>
2022-04-06 14:37:47 -0600

    Inputting multipul numbers and operators now works. Operating on the previous solution now works. Inputting a new number after operating begins a new calculation. The clear button now works.

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
 const runningArray = [];
 const inputArray = [];
+let numberArray = [];
+let operatorArray = [];
+let holdFinal;
 let gettingNumber;
 let calcDisplay = document.querySelector('.calcDisplay');
 
+function calculate(){
+    inputArray.push(runningArray.join(''));
+    getNumberArray();
+    getOperatorArray();
+    let finalValue = numberArray[0];
+    for(let i = operatorArray.length,x = 0;i>0;--i,++x){
+        finalValue = operate(finalValue,numberArray[x+1],operatorArray[x])
+    }
+    refreshDisplay(finalValue);
+    holdFinal = finalValue;
+    console.log('Calculated.')
+    console.log('runningArray:',runningArray,'inputArray',inputArray,'gettingNumber variable',gettingNumber);
+    console.log('finalValue:',finalValue);
+    console.log('holdFinal',holdFinal);
+};
+
+function clearCalc(){
+    const Arrays = [runningArray,inputArray,numberArray,operatorArray];
+    Arrays.forEach(clearArray);
+    holdFinal = undefined;
+    gettingNumber = undefined;
+    refreshDisplay('DISPLAY');
+    console.log('Calculator cleared.');
+};
+function getNumberArray(){
+    numberArray = inputArray.filter((arrayElement)=>{
+        if(isNaN(arrayElement)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
+};
+function getOperatorArray(){
+    operatorArray = inputArray.filter((arrayElement)=>{
+        if(!(isNaN(arrayElement))){
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
+};
 
 function getInput(x){
     //Finalizing number/clearing array and switching to operator selection
     if(isNaN(x)&&gettingNumber==true){
-        console.log('number has been finalized and array has been cleared!')
         gettingNumber = false;
         inputArray.push(runningArray.join(''));
-        clearArray();
+        clearArray(runningArray);
         refreshDisplay(runningArray);
     }
     //Selecting operator
@@ -18,114 +64,67 @@ function getInput(x){
         if(inputArray.length==0){
             return;
         };
-        console.log('operator pushed to running array!')
+        if(holdFinal!==undefined){
+            let finalToInput = holdFinal;
+            clearCalc();
+            inputArray.push(finalToInput);
+            gettingNumber = false;
+        };
         runningArray.push(x);
         refreshDisplay(runningArray[runningArray.length-1]);
     }
     //Finalizing operator/clearing array and switching to number selection
     if(!(isNaN(x))&&gettingNumber==false){
-        console.log('operator has been finalized and array has been cleared!')
         gettingNumber = true;
         inputArray.push(runningArray[runningArray.length-1])
-        clearArray();
+        clearArray(runningArray);
         refreshDisplay(runningArray);
     }
     //Selecting number(s)
     if(!(isNaN(x))){
         if(isNaN(runningArray[0])){
-            clearArray();
+            clearArray(runningArray);
+        };
+        if(holdFinal!==undefined){
+            clearCalc();
         };
         gettingNumber = true;
-        console.log('number pushed to running array!')
         runningArray.push(x);
         refreshDisplay(runningArray.join(''));
     }
-    console.log(runningArray,inputArray,gettingNumber);
+    console.log('Getting input.');
+    console.log('runningArray:',runningArray,'inputArray',inputArray,'gettingNumber variable',gettingNumber);
 };
-function clearArray(){
-    for(let i = runningArray.length-1;i>=0;--i){
-        runningArray.pop();
+
+function clearArray(x){
+    for(let i = x.length-1;i>=0;--i){
+        x.pop();
     };
 };
 function refreshDisplay(x){
     calcDisplay.textContent = x;
 };
-// function getValue(x){
-//     checkArray('checkOperator');
-//     runningArray.push(x);
-// };
-// function getOperator(x){
-//     checkArray('checkValue');
-//     runningArray.push(x);
-// };
-// function checkArray(x){
-//     if(runningArray.length==0){
-//         return;
-//     }
-//     else{
-//         if(x=='checkOperator'){
-//             finalizeOperator();
-//             clearArray(runningArray);
-//         }
-//         else if(x=='checkValue'){
-//             finalizeValue()
-//             clearArray(runningArray);
-//         }
-//         else{
-//             return;
-//         };
-//     };
-// };
-// function finalizeValue(){
-//     inputArray.push(runningArray.join(''));
-// };
-// function finalizeOperator(){
-//     inputArray.push(runningArray[runningArray.length-1]);
-// }
-// function display(){
 
-// };
-
-// function getValue(x){
-//     if(operator!==''){
-//         value2.push(x);
-//         console.log(value1,value2,operator);
-//     }
-//     else{
-//         value1.push(x);
-//         console.log(value1,value2,operator);
-//     }
-//     refreshDisplay();
-// };
-// function getOperator(x){
-
-//     operator = x;
-//     console.log(value1,value2,operator);
-//     refreshDisplay();
-// };
-// function refreshDisplay(){
-//     let display = [...value1,...operator,...value2];
-//     calcDisplay.textContent = display.join('');
-//     console.log(display);
-// };
-function operate(o,a,b){
-    if(o==add){
-        return add(a,b);
+function operate(value1,value2,operator){
+    if(operator=='+'){
+        return add(value1,value2);
     }
-    else if(o==subtract){
-        return subtract(a,b);
+    else if(operator=='-'){
+        return subtract(value1,value2);
     }
-    else if(o==multiply){
-        return multiply(a,b);
+    else if(operator=='*'){
+        return multiply(value1,value2);
     }
-    else if(o==divide){
-        return divide(a,b);
+    else if(operator=='/'){
+        return divide(value1,value2);
     }
     else{
         return;
     };
 };
 function add(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
     return a+b;
 };
 function subtract(a,b){
@@ -137,3 +136,8 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 };
+// let testObject = {
+//     subtract:function(a,b){
+//         return a-b;
+//     },
+// };
